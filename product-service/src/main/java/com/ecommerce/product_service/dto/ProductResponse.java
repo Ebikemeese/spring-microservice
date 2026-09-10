37f77b4fedfd3fd9bf26612dfed3d3e78dc94e4e
Ebikemeese <ebikemeese257@gmail.com>
2026-09-10 16:23:09 +0100

    feat: Add product-service microservice

---
+++ product-service/src/main/java/com/ecommerce/product_service/dto/ProductResponse.java
@@ -0,0 +1,17 @@
+package com.ecommerce.product_service.dto;
+
+import java.math.BigDecimal;
+
+import lombok.Data;
+
+@Data
+public class ProductResponse {
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private Integer stockQuantity;
+    private String category;
+    private String imageUrl;
+    private boolean active;    
+}
